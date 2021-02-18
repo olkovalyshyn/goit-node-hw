0b1f633ad89daf5919349731371fc503e53f077e
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2021-02-18 23:55:27 +0200

    Add scripts

---
+++ contacts.js
@@ -0,0 +1,62 @@
+const fs = require("fs").promises;
+const path = require("path");
+const shortid = require("shortid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+function listContacts() {
+  try {
+    fs.readFile(contactsPath, "utf-8").then((data) => {
+      console.table(JSON.parse(data));
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function getContactById(contactId) {
+  try {
+    fs.readFile(contactsPath, "utf-8").then((data) => {
+      console.table(
+        JSON.parse(data).find((contact) => contact.id === contactId)
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function removeContact(contactId) {
+  try {
+    fs.readFile(contactsPath, "utf-8").then((data) => {
+      const ListContactsWihtoutRemove = JSON.parse(data).filter(
+        (contact) => contact.id !== contactId
+      );
+      fs.writeFile(contactsPath, JSON.stringify(ListContactsWihtoutRemove));
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function addContact(name, email, phone) {
+  try {
+    fs.readFile(contactsPath, "utf-8").then((data) => {
+      const parsedData = JSON.parse(data);
+
+      const newContact = {
+        id: shortid.generate(),
+        name,
+        email,
+        phone,
+      };
+
+      const newBase = [...parsedData, newContact];
+      console.table(newBase);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
